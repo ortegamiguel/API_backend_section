f7a97712d79293dc9217abcdc49ee7cb4578c6b9
Miguel Ortega <omiguel1992@gmail.com>
2020-04-13 12:24:37 -0400

    ADD: initial architecture

---
+++ src/routes/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
+require('express-async-errors');
+const { ErrorMiddleware, NotFoundMiddleware } = require('../middlewares'); 
+
+
+module.exports = ({HomeRoutes}) => {
+    const router = express.Router();
+    const apiRoutes = express.Router();
+
+
+    apiRoutes
+        .use(express.json())
+        .use(cors())
+        .use(helmet())
+        .use(compression());
+
+    apiRoutes.use('/home', HomeRoutes);
+
+    router.use('/v1/api', apiRoutes);
+
+    router.use(ErrorMiddleware);
+    router.use(NotFoundMiddleware);
+
+    return router;
+}
