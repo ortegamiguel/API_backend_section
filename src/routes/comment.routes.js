fa753ecc75a34ea450bc0d7b1e9bfd8478e047c9
Miguel Ortega <omiguel1992@gmail.com>
2020-04-15 18:54:05 -0400

    FIXED: fix endpoint

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -8,7 +8,7 @@ module.exports = ({CommentController}) => {
     router.get('/:commentId/unique', CommentController.get);
     router.get('/:ideaId', CommentController.getIdeaComments);
     router.post('/:ideaId', AuthMiddleware, CommentController.createComment);
-    router.patch('/:commentId', AuthMiddleware,  CommentController.update);
+    router.patch('/:commentId', AuthMiddleware, CommentController.update);
     router.delete('/:commentId', AuthMiddleware, CommentController.delete);
 
     return router;
