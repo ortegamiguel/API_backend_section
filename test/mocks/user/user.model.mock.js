d411dc2371c1f127a54a0dfc0c5a37011316a476
Miguel Ortega <omiguel1992@gmail.com>
2020-04-15 11:31:53 -0400

    ADD: test example added

---
+++ test/mocks/user/user.model.mock.js
@@ -0,0 +1,22 @@
+module.exports = {
+    user: {
+        _id: '5e9631086ef0d017c2cdb730',
+        name: 'miguel',
+        username: 'miguel',
+        password: 'miguel'
+    },
+    users: [
+        {
+            _id: '5e9631086ef0d017c2cdb730',
+            name: 'miguel',
+            username: 'miguel',
+            password: 'miguel'
+        },
+        {
+            _id: '5e9631a885897917f2ff02a5',
+            name: 'angel',
+            username: 'angel',
+            password: 'angel'
+        }
+    ]
+};
---
+++ test/unit/repositories/user.repositorory.test.js
@@ -0,0 +1,20 @@
+const {UserRepository} = require('../../../src/repositories');
+const mockingoose = require('mockingoose').default;
+const {User} = require('../../../src/models');
+let { UserModelMock: { users, user } } = require('../../mocks');
+
+describe('User repository test', () => {
+    beforeEach(() => {
+        mockingoose.resetAll();
+        jest.clearAllMocks();
+    });
+
+    it('Should return user by id', async () => {
+        const _user = { ...user };
+        delete _user.password;
+        mockingoose(User).toReturn(user, 'findOne');
+        const _userRepository = new UserRepository({User});
+        const expected = await _userRepository.get(_user._id);
+        expect(JSON.parse(JSON.stringify(expected))).toMatchObject(_user);
+    });
+})
