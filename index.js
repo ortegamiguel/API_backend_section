bb301564e91db21cd64e1a167b1a7b67188ef9fe
Miguel Ortega <omiguel1992@gmail.com>
2020-04-17 11:16:23 -0400

    fix port

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const { MONGO_URI } = container.resolve('config');
 
 const mongoose = require('mongoose');
 mongoose.set('useCreateIndex', true);
-mongoose.connect(MONGO_URI, { useFindAndModify: false })
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true })
     .then(() => server.start())
     .catch( console.log);
