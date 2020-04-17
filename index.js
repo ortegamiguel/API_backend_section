d1aae32b58a0cbcfc97cf29e8a06e257331919dd
Miguel Ortega <omiguel1992@gmail.com>
2020-04-17 11:27:30 -0400

    fix mongo conection

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const { MONGO_URI } = container.resolve('config');
 
 const mongoose = require('mongoose');
 mongoose.set('useCreateIndex', true);
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true })
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
     .then(() => server.start())
     .catch( console.log(MONGO_URI));
